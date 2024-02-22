d5554590c2c9ebeadb3bf58f5003467ce6590ce6
ponderingdemocritus <ponderingdemocritus@protonmail.com>
2024-02-22 15:53:01 +1100

    details

--- vocs.config.ts
+++ vocs.config.ts
@@ -1,23 +1,19 @@
 import { defineConfig } from "vocs";
 
 export default defineConfig({
-  title: "Realms World Development",
+  title: "Realms World Network",
   sidebar: [
     {
       text: "About",
       link: "/about",
     },
     {
-      text: "Architecture",
-      link: "/architecture",
-    },
-    {
       text: "Getting Started",
       link: "/getting-started",
     },
     {
-      text: "Nodes",
-      link: "/node",
+      text: "Economics",
+      link: "/economics",
     },
     {
       text: "Roadmap",
